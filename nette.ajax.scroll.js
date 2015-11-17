54ccc4b5979814a602aadbc2a0621d6784ba23f6
Vít Kutný <vitkutny@icloud.com>
2015-11-17 22:46:43 +0100

    scroll only to location.hash + refactor

--- nette.ajax.scroll.js
+++ nette.ajax.scroll.js
@@ -1,26 +1,26 @@
 (function (window, $) {
 	$.nette.ext('scroll', {
-		success: function (payload) {
-			var offset = $(window.location.hash || this.target(payload)).offset();
-			if (offset) {
-				$(this.selector).bind(this.events, function (event) {
-					event.preventDefault();
-				});
-				var that = this;
-				$(this.selector).stop().animate({
-					scrollTop: offset.top,
-					scrollLeft: offset.left
-				}, this.speed, function () {
-					$(that.selector).unbind(that.events);
-				});
+		success: function () {
+			var hash = window.location.hash;
+			if (hash) {
+				var offset = $(hash).offset();
+				if (offset) {
+					var that = this;
+					this.element.on(this.stopEvents, function () {
+						that.element.stop();
+					});
+					this.element.stop().animate({
+						scrollTop: offset.top,
+						scrollLeft: offset.left
+					}, this.speed, function () {
+						that.element.off(that.stopEvents);
+					});
+				}
 			}
 		}
 	}, {
 		speed: 'normal',
-		selector: 'html, body',
-		events: 'scroll mousedown DOMMouseScroll mousewheel keyup',
-		target: function (payload) {
-			return payload.snippets && '#' + Object.keys(payload.snippets).shift();
-		}
+		element: $('html, body'),
+		stopEvents: 'scroll.stop mousedown.stop wheel.stop DOMMouseScroll.stop mousewheel.stop keyup.stop touchmove.stop',
 	});
 })(window, window.jQuery);
